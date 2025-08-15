9a122eb95eba625dbf0e204755b320d05e416525
Aishwarya Ghosh Dastidar <hustleaish7@gmail.com>
2025-08-16 01:29:50 +0530

    Added IPL analysis project files

---
+++ .ipynb_checkpoints/ipl_analysis-checkpoint.py
@@ -0,0 +1,4 @@
+import pandas as pd
+matches = pd.read_csv(r"C:\Users\HP\Downloads\matches.csv.zip")
+deliveries=pd.read_csv(r"C:\Users\HP\Downloads\deliveries.csv.zip")
+print(matches.head(5))
---
+++ ipl_analysis.py
@@ -0,0 +1,5 @@
+import pandas as pd
+matches = pd.read_csv(r"C:\Users\HP\Downloads\matches.csv.zip")
+deliveries=pd.read_csv(r"C:\Users\HP\Downloads\deliveries.csv.zip")
+print(matches.head(5))
+print(matches.isnull().sum())
